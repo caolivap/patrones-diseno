78d2503a59616f685b221538e40556dbf274fdb1
Carlos Oliva <colivap@devco.com.co>
2020-01-04 20:29:04 -0500

    Se crea rama dependency-injection

--- DAO/src/main/java/com/mitocode/dao/MedicoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.mitocode.dao;
-
-import com.mitocode.model.Medico;
-
-public interface MedicoDAO extends CRUD<Medico>{
-
-	void curar();
-
-}
--- Prototype/src/main/java/com/mitocode/inter/ICuenta.java
+++
@@ -1,7 +0,0 @@
-package com.mitocode.inter;
-
-public interface ICuenta extends Cloneable {
-
-	ICuenta clonar();
-
-}
